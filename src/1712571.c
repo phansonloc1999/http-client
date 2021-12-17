55de9db305a8812df593975aefde867cecdde293
phansonloc1999 <phansonloc1999@gmail.com>
2021-12-17 08:16:24 +0700

    Add receiveResponse()

--- src/1712571.c
+++ src/1712571.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -19,6 +20,47 @@ void printUsage(int argc)
 		printf("Example: ./1712571 example.com/index.html index.html\n");
 		exit(0);
 	}
+}
+
+void receiveResponse(int sock, int outfile)
+{
+	const int bufferSize = 16;
+	char buffer[bufferSize];
+	int receivedLen = 0, contentLength;
+	char *response = (char *)malloc(0);
+	char *contentLengthStr = NULL;
+	char *dataPtr = NULL;
+	bool skip = false;
+
+	while ((receivedLen = recv(sock, buffer, bufferSize - 1, 0)) > 0)
+	{
+		buffer[receivedLen] = '\0'; // Last is the null terminating character
+		response = (char *)realloc(response, strlen(response) + strlen(buffer) + 1);
+		sprintf(response, "%s%s", response, buffer); // Append buffer to response
+
+		if (skip == false)
+		{
+			if (dataPtr == NULL)
+			{
+				dataPtr = strstr(response, "\r\n\r\n"); // Find the end of response header
+			}
+			else
+			{
+				// Get content length value after the character ':'
+				contentLengthStr = strstr(response, "Content-Length");
+				if (contentLengthStr != NULL)
+				{
+					contentLength = atoi((char *)(strchr(contentLengthStr, ':') + 1));
+				}
+
+				dataPtr = dataPtr + 4; // Move right 4 bytes to skip \r\n\r\n
+
+				skip = true;
+			}
+		}
+	}
+
+	write(outfile, dataPtr, strlen(dataPtr));
 }
 
 int main(int argc, char const *argv[])
@@ -62,46 +104,25 @@ int main(int argc, char const *argv[])
 	if (connect(sock, result->ai_addr, result->ai_addrlen) == -1)
 		printf("connect failed");
 
-	request = (char*)malloc(1000);
+	request = (char *)malloc(1000);
 
 	sprintf(request, "GET /%s HTTP/1.1\nHost: %s\nUser-agent: my http client\n\n", rest, domain);
 	printf("%s\n\n\n\n", request);
 
 	write(sock, request, strlen(request));
 
-	shutdown(sock, SHUT_WR); // Prevent further writing data to socket 
+	shutdown(sock, SHUT_WR); // Prevent further writing data to socket
 
 	int outfile;
 	if (argc == 3)
 	{
 		outfile = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-		if (outfile == -1) printf("open %s file to output failed", argv[2]);
+		if (outfile == -1)
+			printf("open %s file to output failed", argv[2]);
 	}
 
-	const int bufferSize = 16;
-	char buffer[bufferSize];
-	int receivedLen = 0, contentLength;
-	char *response = (char*)malloc(0);
-	char *contentLengthStr = NULL;
+	receiveResponse(sock, outfile);
 
-	while ((receivedLen = recv(sock, buffer, bufferSize-1, 0)) > 0)
-	{
-		buffer[receivedLen] = '\0';
-		response = (char*)realloc(response, strlen(response) + strlen(buffer) + 1);
-		sprintf(response, "%s%s", response, buffer);	
-
-		if (strstr(response, "\r\n\r\n") != NULL)
-		{
-			contentLengthStr = strstr(response, "Content-Length");
-			if (contentLengthStr != NULL)
-			{
-				contentLength = atoi((char*)(strchr(contentLengthStr,':') + 1));
-				printf("%d\n", contentLength);
-				// exit(0);
-			}
-		}
-	}
-	
 	close(sock);
 	close(outfile);
 
