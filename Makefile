9441dd3fc654bcb43c40daa0a26293129f9b8c69
phansonloc1999 <phansonloc1999@gmail.com>
2021-12-19 11:09:20 +0700

    Add Report, rename output folder to exe

--- Makefile
+++ Makefile
@@ -3,15 +3,15 @@ MSSV = 1712571
 all: $(MSSV)
 
 $(MSSV): src/$(MSSV).c
-	mkdir -p output
-	gcc src/$(MSSV).c -o output/$(MSSV) -lm
+	mkdir -p exe
+	gcc src/$(MSSV).c -o exe/$(MSSV) -lm
 
 clean:
-	rm -rf output/*
+	rm -rf exe/*
 
 run: $(MSSV)
-	./output/$(MSSV) http://jigsaw.w3.org/HTTP/ChunkedScript output/jigsaw_chunkedscript.html
-	./output/$(MSSV) http://example.com/index.html output/example.com_index.html
-	./output/$(MSSV) http://anglesharp.azurewebsites.net/Chunked output/azure_chunked.html
-	./output/$(MSSV) http://info.cern.ch/ output/infocern_index.html
-	@echo "Done! Check output folder"
+	./exe/$(MSSV) http://jigsaw.w3.org/HTTP/ChunkedScript exe/jigsaw_chunkedscript.html
+	./exe/$(MSSV) http://example.com/index.html exe/example.com_index.html
+	./exe/$(MSSV) http://anglesharp.azurewebsites.net/Chunked exe/azure_chunked.html
+	./exe/$(MSSV) http://info.cern.ch/ exe/infocern_index.html
+	@echo "Done! Check exe folder"
--- check.sh
+++ check.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 printf "Checking jigsaw chunked script"
-curl -v http://jigsaw.w3.org/HTTP/ChunkedScript -o output/result.html &> /dev/null && \
-diff output/result.html output/jigsaw_chunkedscript.html
+curl -v http://jigsaw.w3.org/HTTP/ChunkedScript -o exe/result.html &> /dev/null && \
+diff exe/result.html exe/jigsaw_chunkedscript.html
 
 printf "\n\nChecking example.com"
-curl -v http://example.com -o output/result.html &> /dev/null && \
-diff output/result.html output/example.com_index.html
+curl -v http://example.com -o exe/result.html &> /dev/null && \
+diff exe/result.html exe/example.com_index.html
 
 printf "\n\nChecking azurewebsites chunked"
-curl -v http://anglesharp.azurewebsites.net/Chunked -o output/result.html &> /dev/null && \
-diff output/result.html output/azure_chunked.html
+curl -v http://anglesharp.azurewebsites.net/Chunked -o exe/result.html &> /dev/null && \
+diff exe/result.html exe/azure_chunked.html
 
 printf "\n\nChecking info cern"
-curl -v http://info.cern.ch/ -o output/result.html &> /dev/null && \
-diff output/result.html output/infocern_index.html
+curl -v http://info.cern.ch/ -o exe/result.html &> /dev/null && \
+diff exe/result.html exe/infocern_index.html
